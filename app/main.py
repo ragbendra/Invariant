b9755cf93d9314821951c4565bfd3f5fe894e8fc
Raghabendra K.  Shah <raghabendrashah.1234@gmail.com>
2026-07-23 14:59:58 +0545

    feat(ui): implement Jinja2 template

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from app.routers import posts
 
-app = FastAPI(title="Blog App")
+app = FastAPI(title="Invariant Blog App")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(posts.router)
 
